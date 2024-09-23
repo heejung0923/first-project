58f799991aeb208980a0b6d683ee911262bfd5e1
ni-heejung <heejung.im@nextintelligence.ai>
2024-09-23 17:47:23 +0900

    ✨feat: antd 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { ConfigProvider } from 'antd';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import IndexPage from '@/pages/page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <IndexPage />,
+  },
+]);
+
 function App() {
-  return <>sㄴㄴs</>;
+  return (
+    <ConfigProvider>
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
+};
+
+export default IndexPage;
